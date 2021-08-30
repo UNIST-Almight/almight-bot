d14aa62e61550dfd37cb880fd1b5f08605561ff8
JinLover <51523331+JinLover@users.noreply.github.com>
2021-08-30 14:25:24 +0900

    now bot can reply!

--- main.py
+++ main.py
@@ -8,9 +8,73 @@ app = App(
     token=os.environ.get("SLACK_BOT_TOKEN"),
     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
-
 # Add functionality here
 # @app.event("app_home_opened") etc
 
+@app.event("message")
+def handle_message_events(body, logger):
+    logger.info(body)
+    print(body)
+
+@app.event("app_mention")
+def event_test(body, say, logger):
+    logger.info(body)
+    say("What's up?")
+
+@app.message(":wave:")
+def say_hello(message, say):
+    user = message['user']
+    say(f"Hi there, <@{user}>!")
+
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+    try:
+        # views.publish is the method that your app uses to push a view to the Home tab
+        client.views_publish(
+        # the user that opened your app's app home
+        user_id=event["user"],
+        # the view object that appears in the app home
+        view={
+            "type": "home",
+            "callback_id": "home_view",
+
+            # body of the view
+            "blocks": [
+            {
+                "type": "section",
+                "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome to your _App's Home_* :tada:"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                "type": "mrkdwn",
+                "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                    "type": "plain_text",
+                    "text": "Click me!"
+                    }
+                }
+                ]
+            }
+            ]
+        }
+        )
+  
+    except Exception as e:
+        logger.error(f"Error publishing home tab: {e}")
+
 if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
+    app.start(port=int(os.environ.get("PORT", 5000)))
